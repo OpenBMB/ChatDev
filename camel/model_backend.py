4079b9e0f12cad16f24252721721be532934dde7
Kieran Gill <kieran.gill1997@gmail.com>
2023-10-05 20:22:11 -0400

    make reported cost based on the model used

--- camel/model_backend.py
+++ camel/model_backend.py
@@ -18,6 +18,7 @@ import openai
 import tiktoken
 
 from camel.typing import ModelType
+from chatdev.statistics import prompt_cost
 from chatdev.utils import log_and_print_online
 
 
@@ -69,11 +70,16 @@ class OpenAIModel(ModelBackend):
         response = openai.ChatCompletion.create(*args, **kwargs,
                                                 model=self.model_type.value,
                                                 **self.model_config_dict)
+        cost = prompt_cost(
+                self.model_type.value, 
+                num_prompt_tokens=response["usage"]["prompt_tokens"], 
+                num_completion_tokens=response["usage"]["completion_tokens"]
+        )
 
         log_and_print_online(
-            "**[OpenAI_Usage_Info Receive]**\nprompt_tokens: {}\ncompletion_tokens: {}\ntotal_tokens: {}\n".format(
+            "**[OpenAI_Usage_Info Receive]**\nprompt_tokens: {}\ncompletion_tokens: {}\ntotal_tokens: {}\ncost: ${:.6f}\n".format(
                 response["usage"]["prompt_tokens"], response["usage"]["completion_tokens"],
-                response["usage"]["total_tokens"]))
+                response["usage"]["total_tokens"], cost))
         if not isinstance(response, Dict):
             raise RuntimeError("Unexpected return from OpenAI API")
         return response
