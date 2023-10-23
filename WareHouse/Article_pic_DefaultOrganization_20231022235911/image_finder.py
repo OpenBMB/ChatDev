0b9f23049e8e4bbd36c797e9d32c6d287d226b62
yudong mei <201436009@uibe.edu.cn>
2023-10-23 11:04:59 +0800

    modelbest1024

--- WareHouse/Article_pic_DefaultOrganization_20231022235911/image_finder.py
+++
@@ -1,8 +0,0 @@
-'''
-This file contains the ImageFinder class that is responsible for finding a suitable image for the article.
-'''
-class ImageFinder:
-    def find_image(self, article_text):
-        # Implement logic to find a suitable image based on the article text
-        # Return the URL of the found image
-        pass
