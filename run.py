e8db0c2d0628f156a02660bb92491a6e8e436196
Alexander Somma <admin@openco.ca>
2023-11-03 18:47:44 -0100

    Fixing Copyright for out team's work

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-# =========== Copyright 2023 @ CAMEL-AI.org. All Rights Reserved. ===========
+# =========== Copyright 2023 Startr.LLC & CAMEL-AI.org. All Rights Reserved. ===========
 # Licensed under the Apache License, Version 2.0 (the “License”);
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -10,7 +10,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# =========== Copyright 2023 @ CAMEL-AI.org. All Rights Reserved. ===========
+# =========== Copyright 2023 @ Startr.LLC & CAMEL-AI.org. All Rights Reserved. ===========
 import argparse
 import logging
 import os
