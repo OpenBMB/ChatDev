9b6eee8182a3613d5a7e5fad2f79cc3dbacad0e4
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2023-12-08 14:55:24 -0700

    Clean up CLI args

--- run.py
+++ run.py
@@ -63,17 +63,17 @@ def get_config(company: str) -> Tuple[str, str, str]:
 
 parser = argparse.ArgumentParser(description='argparse')
 parser.add_argument('--config', type=str, default="Default",
-                    help="Name of config, which is used to load configuration under CompanyConfig/")
+                    help="Name of config (subdir in  CompanyConfig/)")
 parser.add_argument('--org', type=str, default="DefaultOrganization",
-                    help="Name of organization, your software will be generated in WareHouse/name_org_timestamp")
+                    help="Name of organization (your software will be generated in WareHouse/name_org_timestamp)")
 parser.add_argument('--task', type=str, default="Develop a basic Gomoku game.",
                     help="Prompt of software")
 parser.add_argument('--name', type=str, default="Gomoku",
-                    help="Name of software, your software will be generated in WareHouse/name_org_timestamp")
-parser.add_argument('--model', type=str, default="GPT_3_5_TURBO",
+                    help="Name of software (your software will be generated in WareHouse/name_org_timestamp)")
+parser.add_argument('--model', type=str, default="GPT_3_5_TURBO", choices=['GPT_3_5_TURBO', 'GPT_4', 'GPT_4_32K'],
                     help="GPT Model, choose from {'GPT_3_5_TURBO','GPT_4','GPT_4_32K'}")
 parser.add_argument('--path', type=str, default="",
-                    help="Your file directory, ChatDev will build upon your software in the Incremental mode")
+                    help="Your file directory. If given, ChatDev will build upon your software in the Incremental mode.")
 args = parser.parse_args()
 
 # Start ChatDev
