8cd4be730fe4b504e6b2dbe61aacbc8ac625c53e
rizzleroc <rizzleroc@gmail.com>
2024-05-13 22:31:37 -0700

    Add files via upload

--- camel/model_backend.py
+++ camel/model_backend.py
@@ -92,7 +92,7 @@ class OpenAIModel(ModelBackend):
                 "gpt-4-32k": 32768,
                 "gpt-4-1106-preview": 4096,
                 "gpt-4-1106-vision-preview": 4096,
-                "gpt-4o": 8192,
+                "gpt-4o": 4096,
             }
             num_max_token = num_max_token_map[self.model_type.value]
             num_max_completion_tokens = num_max_token - num_prompt_tokens
