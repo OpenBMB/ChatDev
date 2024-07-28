44b1955993d764e755b96efb0c1fe52c82a3f614
OpenCo <admin@openco.ca>
2024-07-28 10:31:51 +0000

    Create landing page for consulting team using startr.style

---
+++ WareHouse/Gomoku_DefaultOrganization_20240728102334/landing_page.py
@@ -0,0 +1,5 @@
+import tkinter as tk
+class LandingPage(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        # Add code to create and configure the widgets for the landing page
